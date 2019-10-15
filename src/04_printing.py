f804805737b592a79bfdb9dfe21b96fbebc8f251
David Situ <davidsitu626@gmail.com>
2019-10-14 23:58:15 -0700

    Printing and Lists Complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,11 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+# %[flags][width][.precision]type 
+print('printf -- ', 'X: %4i, Y: %4.5f, Z: %s' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+print('format -- ','X:   {0}, Y: {1}, Z: {2}'.format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print(f'f-stri -- X:    {x}, Y:    {y}, Z: {z}')
